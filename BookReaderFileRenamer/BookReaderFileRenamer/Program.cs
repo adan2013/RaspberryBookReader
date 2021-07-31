cd20272de8b9c10025644acb8334a0b46b11ad07
Daniel Alberski <danielalberski@gmail.com>
2021-07-31 19:05:25 +0200

    recursive search

--- BookReaderFileRenamer/BookReaderFileRenamer/Program.cs
+++ BookReaderFileRenamer/BookReaderFileRenamer/Program.cs
@@ -17,6 +17,42 @@ namespace BookReaderFileRenamer
             return "00" + index.ToString() + ".mp3";
         }
 
+        static void renameFilesInDirectory(string path)
+        {
+            Console.WriteLine("------------------------------");
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            FileInfo[] files = dirInfo.GetFiles().Where(p => p.Extension == ".mp3").OrderBy(p => p.Name).ToArray();
+            if(files.Length == 0)
+            {
+                Console.WriteLine("Directory: " + path);
+                Console.WriteLine("Directory doesn't contain any files!");
+            }
+            else
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo file = files[i];
+                    Console.WriteLine(file.Name + " >>> " + generateFileName(i));
+                }
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Directory: " + path);
+                Console.WriteLine("Type \"yes\" to accept this file names: ");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        files[i].MoveTo(Path.Combine(path, generateFileName(i)));
+                    }
+                    Console.WriteLine("Done! Files renamed successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Operation abort!");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             if (args.Length > 0)
@@ -24,26 +60,18 @@ namespace BookReaderFileRenamer
                 string dirPath = args[0];
                 if(Directory.Exists(dirPath))
                 {
+                    renameFilesInDirectory(dirPath);
                     DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
-                    FileInfo[] files = dirInfo.GetFiles().OrderBy(p => p.Name).ToArray();
-                    for (int i = 0; i < files.Length; i++)
+                    DirectoryInfo[] directories = dirInfo.GetDirectories().OrderBy(p => p.Name).ToArray();
+                    if(directories.Length > 0)
                     {
-                        FileInfo file = files[i];
-                        Console.WriteLine(file.Name + " >>> " + generateFileName(i));
-                    }
-                    Console.WriteLine("Type \"yes\" to accept this file names: ");
-                    string answer = Console.ReadLine();
-                    if(answer == "yes")
-                    {
-                        for (int i = 0; i < files.Length; i++)
+                        Console.WriteLine("------------------------------");
+                        Console.WriteLine("Running recursive search...");
+                        for (int i = 0; i < directories.Length; i++)
                         {
-                            files[i].MoveTo(Path.Combine(dirPath, generateFileName(i)));
+                            DirectoryInfo d = directories[i];
+                            renameFilesInDirectory(d.FullName);
                         }
-                        Console.WriteLine("Done! Files renamed successfully!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Operation abort!");
                     }
                 }
                 else
